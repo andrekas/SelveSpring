72c79e099c5f408684de5580b92d9ed022002e74
andrekas <alaurimaa@gmail.com>
2018-03-13 11:54:35 +0200

    springboot

---
+++ src/main/java/ee/ttu/tarkvaratehnika/selveleidja/SelveleidjaApplication.java
@@ -0,0 +1,12 @@
+package ee.ttu.tarkvaratehnika.selveleidja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelveleidjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelveleidjaApplication.class, args);
+	}
+}
---
+++ src/test/java/ee/ttu/tarkvaratehnika/selveleidja/SelveleidjaApplicationTests.java
@@ -0,0 +1,16 @@
+package ee.ttu.tarkvaratehnika.selveleidja;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SelveleidjaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
